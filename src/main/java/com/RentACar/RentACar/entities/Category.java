41d4f8e04329209b26464422f2361067bfb9d2b5
IvelinP30 <ivo3004@abv.bg>
2023-12-27 17:39:14 +0200

    Add more entities
    Car and Category

---
+++ src/main/java/com/RentACar/RentACar/entities/Category.java
@@ -0,0 +1,42 @@
+package com.RentACar.RentACar.entities;
+
+import org.antlr.v4.runtime.misc.NotNull;
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "categories")
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name = "name")
+    @NotNull
+    private String name;
+
+    @OneToOne(mappedBy = "categories")
+    private User user;
+
+    public Category() {
+    }
+
+    public Category(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+}
