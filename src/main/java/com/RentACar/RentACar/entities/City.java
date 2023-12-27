a03b6ad2f16607b268c95a4e5c1c7a3893cca261
IvelinP30 <ivo3004@abv.bg>
2023-12-27 17:35:32 +0200

    Add entities and application properties
    City and User entities

---
+++ src/main/java/com/RentACar/RentACar/entities/City.java
@@ -0,0 +1,44 @@
+package com.RentACar.RentACar.entities;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import jakarta.persistence.*;
+import java.util.Set;
+
+@Entity
+@Table(name = "city")
+public class City {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @JsonIgnore
+    private Long id;
+
+    private String name;
+
+    @JsonIgnore
+    @OneToMany(mappedBy = "city")
+    private Set<User> clients;
+
+    public City(){
+    }
+
+    public City(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Set<User> getClients() {
+        return clients;
+    }
+
+    public void setClients(Set<User> clients) {
+        this.clients = clients;
+    }
+}
