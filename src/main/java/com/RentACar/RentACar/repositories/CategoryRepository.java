9dcc729f361bba106cdf6f15c4c7e2b8bb202a98
IvelinP30 <133099999+IvelinP30@users.noreply.github.com>
2024-01-04 14:10:09 +0200

    Initial base fill with Brand, Fuel, Category, City

--- src/main/java/com/RentACar/RentACar/repositories/CategoryRepository.java
+++ src/main/java/com/RentACar/RentACar/repositories/CategoryRepository.java
@@ -4,4 +4,5 @@ import com.RentACar.RentACar.entities.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CategoryRepository extends JpaRepository<Category, Long> {
+    Category findCategoryByName(String name);
 }
--- src/main/java/com/RentACar/RentACar/repositories/FuelRepository.java
+++ src/main/java/com/RentACar/RentACar/repositories/FuelRepository.java
@@ -4,4 +4,5 @@ import com.RentACar.RentACar.entities.Fuel;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface FuelRepository extends JpaRepository<Fuel, Long> {
+    Fuel findFuelByName(String name);
 }
