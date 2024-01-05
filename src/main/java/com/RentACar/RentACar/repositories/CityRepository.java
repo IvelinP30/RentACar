da62282bd29c64c57d95442fa152ad988ae22e30
Velimir Ivanov <vivanov@tu-sofia.bg>
2024-01-05 13:46:24 +0200

    Changes in InitialDataLoader

--- src/main/java/com/RentACar/RentACar/repositories/CityRepository.java
+++ src/main/java/com/RentACar/RentACar/repositories/CityRepository.java
@@ -1,10 +1,13 @@
 package com.RentACar.RentACar.repositories;
 
 import com.RentACar.RentACar.entities.City;
+import com.RentACar.RentACar.entities.User;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface CityRepository extends JpaRepository<City, Long> {
 
@@ -16,4 +19,13 @@ public interface CityRepository extends JpaRepository<City, Long> {
     Page<City> filterCity(Pageable pageable, String name);
 
     City findCityByName(String name);
+
+
+    @Query("SELECT u " +
+            "FROM User u " +
+            "INNER JOIN City c ON c.id = u.city.id " +
+            "WHERE " +
+            "lower(c.name) " +
+            "LIKE :#{'%' + cityName + '%'} " )
+    Page<User> findUsersByCityPageable(String cityName, Pageable pageable);
 }
